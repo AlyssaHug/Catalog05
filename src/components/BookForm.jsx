4a87102785c0d5b9ce90e630e207c7a5253ce709
AlyssaHug <AlyssaHug@shaw.ca>
2025-10-20 22:00:12 -0700

    adjusted book form styles

--- src/components/BookForm.jsx
+++ src/components/BookForm.jsx
@@ -42,68 +42,72 @@ function BookForm({ addBook, updateBook, book, closeModal }) {
 
     return (
         <div className='form-container'>
-            <h2 className='formtitle'>Add a new book!</h2>
+            <h2 className='form-title'>Add a new book!</h2>
             <form onSubmit={handleSubmit}>
-                <div className='form-control'>
-                    <label htmlFor='title'>Title:</label>
-                    <input
-                        type='text'
-                        name='title'
-                        placeholder='book title'
-                        defaultValue={initialValues.title}
-                    />
-                </div>
-                <div className='form-control'>
-                    <label htmlFor='author'>Author:</label>
-                    <input
-                        type='text'
-                        name='author'
-                        placeholder='author'
-                        defaultValue={initialValues.author}
-                    />
-                </div>
-                <div className='form-control'>
-                    <label htmlFor='publisher'>Publisher:</label>
-                    <input
-                        type='text'
-                        name='publisher'
-                        placeholder='publisher'
-                        defaultValue={initialValues.publisher}
-                    />
-                </div>
-                <div className='form-control'>
-                    <label htmlFor='publicationYear'>Publication Year:</label>
-                    <input
-                        type='number'
-                        name='publicationYear'
-                        defaultValue={initialValues.year}
-                    />
-                </div>
-                <div className='form-control'>
-                    <label htmlFor='language'>Language:</label>
-                    <input
-                        type='text'
-                        name='language'
-                        placeholder='language'
-                        defaultValue={initialValues.language}
-                    />
-                </div>
-                <div className='form-control'>
-                    <label htmlFor='pages'>Pages:</label>
-                    <input
-                        type='number'
-                        name='pages'
-                        defaultValue={initialValues.pages}
-                    />
-                </div>
-                <div className='form-control'>
-                    <label htmlFor='image'>Image URL:</label>
-                    <input
-                        type='url'
-                        name='image'
-                        placeholder='image URL'
-                        defaultValue={initialValues.url}
-                    />
+                <div className='form-inputs'>
+                    <div className='form-control'>
+                        <label htmlFor='title'>Title:</label>
+                        <input
+                            type='text'
+                            name='title'
+                            placeholder='book title'
+                            defaultValue={initialValues.title}
+                        />
+                    </div>
+                    <div className='form-control'>
+                        <label htmlFor='author'>Author:</label>
+                        <input
+                            type='text'
+                            name='author'
+                            placeholder='author'
+                            defaultValue={initialValues.author}
+                        />
+                    </div>
+                    <div className='form-control'>
+                        <label htmlFor='publisher'>Publisher:</label>
+                        <input
+                            type='text'
+                            name='publisher'
+                            placeholder='publisher'
+                            defaultValue={initialValues.publisher}
+                        />
+                    </div>
+                    <div className='form-control'>
+                        <label htmlFor='publicationYear'>
+                            Publication Year:
+                        </label>
+                        <input
+                            type='number'
+                            name='publicationYear'
+                            defaultValue={initialValues.year}
+                        />
+                    </div>
+                    <div className='form-control'>
+                        <label htmlFor='language'>Language:</label>
+                        <input
+                            type='text'
+                            name='language'
+                            placeholder='language'
+                            defaultValue={initialValues.language}
+                        />
+                    </div>
+                    <div className='form-control'>
+                        <label htmlFor='pages'>Pages:</label>
+                        <input
+                            type='number'
+                            name='pages'
+                            defaultValue={initialValues.pages}
+                        />
+                    </div>
+                    <div className='form-control'>
+                        <label htmlFor='image'>Image URL:</label>
+                        <input
+                            type='url'
+                            name='image'
+                            placeholder='image URL'
+                            defaultValue={initialValues.url}
+                        />
+                    </div>
                 </div>
                 <button
                     className='save'
